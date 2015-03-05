ffe2bfc17a89a476d80bbcf9587a95f91f21f254
Ivan Nikulin <ifaaan@gmail.com>
2015-03-05 13:21:26 +0300

    Test data to JSON converter: add license, keep original file formatting

--- tools/test_data_to_json/test_data_to_json.py
+++ tools/test_data_to_json/test_data_to_json.py
@@ -1,9 +1,73 @@
+"""
+Copyright (c) 2015, Ivan Nikulin (ifaaan@gmail.com)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
 from optparse import OptionParser
+from collections import OrderedDict
 import os
 import sys
 import glob
 import re
 import json
+
+# NOTE we use custom encoder here to keep original formatting in the parser.json
+class TestDataEncoder(json.JSONEncoder):
+    indentUnit = '  '
+
+    def __init__(self, *args, **kwargs):
+        super(TestDataEncoder, self).__init__(*args, **kwargs)
+        self.indent = ''
+
+    def encode(self, o):
+        if isinstance(o, list):
+            if len(o) == 0:
+                return '[]'
+
+            prev_indent = self.indent
+            self.indent += TestDataEncoder.indentUnit
+            encoded_items = [self.indent + self.encode(i) for i in o]
+            self.indent = prev_indent
+            encoded_list = '[\n' + ',\n'.join(encoded_items) + '\n' + self.indent + ']'
+            return encoded_list
+
+        elif isinstance(o, dict):
+            if 'name' in o and 'value' in o:
+                return json.dumps(o)
+
+            prev_indent = self.indent
+            self.indent += TestDataEncoder.indentUnit
+
+            encoded_items = [self.indent + json.dumps(key) + ": " + self.encode(value)
+                             for key, value in o.iteritems()]
+
+            self.indent = prev_indent
+            encoded_dict = '{\n' + ',\n'.join(encoded_items) + '\n' + self.indent + '}'
+            return encoded_dict
+
+        else:
+            return json.dumps(o)
 
 
 def parse_options():
@@ -65,16 +129,18 @@ def read_data_dir(data_dir):
         test_name = re.findall('(.*)-test', file_name)[0]
         expected_file = os.path.join(data_dir, test_name + '-expected')
         location, set_cookies = parse_test_file(test_file)
-        test_case = {
-            'test': test_name.upper(),
-            'received': set_cookies,
-            'sent': parse_expected_file(expected_file)
-        }
+
+        # NOTE: use OrderedDict here to keep fields order in the original parser.json file
+        test_case = [
+            ('test', test_name.upper()),
+            ('received', set_cookies),
+            ('sent', parse_expected_file(expected_file))
+        ]
 
         if location:
-            test_case['sent-to'] = location
+            test_case.insert(2, ('sent-to', location))
 
-        tests.append(test_case)
+        tests.append(OrderedDict(test_case))
 
     return tests
 
@@ -107,7 +173,8 @@ def main():
         os.makedirs(output_dir)
 
     with open(options.output_file, 'wb') as f:
-        json.dump(tests, f, indent=2)
+        data_json = json.dumps(tests, cls=TestDataEncoder)
+        f.write(data_json)
 
     print 'Done'
 
